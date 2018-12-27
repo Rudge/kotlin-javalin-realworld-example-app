7d4aa3ce6f78a37d446ec5b2a0eca9114c0c9546
Rudge Ferreira <rudgee@gmail.com>
2018-12-27 04:10:58 -0200

    Update error handling

--- src/main/kotlin/io/realworld/app/web/ErrorExceptionMapping.kt
+++ src/main/kotlin/io/realworld/app/web/ErrorExceptionMapping.kt
@@ -1,11 +1,11 @@
 package io.realworld.app.web
 
 import com.fasterxml.jackson.annotation.JsonRootName
+import io.javalin.BadRequestResponse
 import io.javalin.Javalin
 import io.javalin.UnauthorizedResponse
 import io.realworld.app.exception.HttpResponseException
 import org.eclipse.jetty.http.HttpStatus
-import java.lang.reflect.InvocationTargetException
 
 @JsonRootName("errors")
 class ErrorResponse : HashMap<String, Any>()
@@ -13,11 +13,17 @@ class ErrorResponse : HashMap<String, Any>()
 object ErrorExceptionMapping {
     fun register(app: Javalin) {
         app.exception(Exception::class.java) { e, ctx ->
-            val error = ErrorResponse()
-            error["Unknow Error"] = listOf(e.message)
-            ctx.json(error).status(HttpStatus.INTERNAL_SERVER_ERROR_500)
+            if (e.cause is BadRequestResponse) {
+                val error = ErrorResponse()
+                error["body"] = "can't be empty"
+                ctx.json(error).status(HttpStatus.UNPROCESSABLE_ENTITY_422)
+            } else {
+                val error = ErrorResponse()
+                error["Unknow Error"] = listOf(e.message)
+                ctx.json(error).status(HttpStatus.INTERNAL_SERVER_ERROR_500)
+            }
         }
-        app.exception(InvocationTargetException::class.java) { e, ctx ->
+        app.exception(BadRequestResponse::class.java) { e, ctx ->
             val error = ErrorResponse()
             error["body"] = "can't be empty"
             ctx.json(error).status(HttpStatus.UNPROCESSABLE_ENTITY_422)
